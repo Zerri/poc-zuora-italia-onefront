99f438af8f4b8bfa91618394eb4d84cf87f50fb8
Javin Barone <j.barone@teamsystem.com>
2024-07-31 15:07:23 +0200

    chore: 💡 add comment explaining appId

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { examples } from "./features/examples";
 
 const proxyBaseUrl = import.meta.env.VITE_APP_PROXY_BASE_URL || "/api";
 
+/*
+  In production, the VITE_APP_ID should be set in the environment variables of the pipeline,
+  The default to demo is needed to redirect to localhost:3000 in development
+*/
 const appId = import.meta.env.VITE_APP_ID || "demo";
 
 OneFront.run({
