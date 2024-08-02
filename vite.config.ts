be077527dae06941fc4b476eab16ebcd8fc4fbc9
Javin Barone <j.barone@teamsystem.com>
2024-08-02 11:18:44 +0200

    feat: ✨ update dependencies and align config and index.html to new features

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
+import { oneFrontApp } from "@1f/vite-plugin"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        target: "http://localhost:4010",
-      },
-    },
-    open: true,
-    port: 3000,
-  },
+  plugins: [react(), oneFrontApp()],
 })
