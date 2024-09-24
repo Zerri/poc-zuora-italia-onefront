713ff854a4e762bd946ea2016da6d2095d347ba7
Etjen Disha <e.disha@teamsystem.com>
2024-09-24 15:41:48 +0200

    chore: :arrow_up: upgarde @1f/react-sdk and @1f/vite-plugin to version 2.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { oneFrontApp } from "@1f/vite-plugin"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { oneFrontApp } from "@1f/vite-plugin"
 
 export default defineConfig({
-  plugins: [react(), oneFrontApp()],
+  plugins: [react(), oneFrontApp()]
 })
